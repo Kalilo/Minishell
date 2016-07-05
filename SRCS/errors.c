d5a61aab23a6aacd0503fe8993b474fbc73f07f3
Kalilo <kalilohansmann@gmail.com>
2016-07-05 11:06:46 +0200

    Added error mesage

--- SRCS/errors.c
+++ SRCS/errors.c
@@ -6,10 +6,16 @@ int	error1(int err)
 		ft_putstr(E_MESS01);
 	else if (err == 2)
 		ft_putstr(E_MESS02);
+	else i f(err == 3)
+		ft_putstr(E_MESS03);
+	else
+		return (0);
+	return (1);
 }
 
 int	error(int err)
 {
 	if (0 <= err && err <= 9)
 		return (error1(err));
+	return (1);
 }
