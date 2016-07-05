cdec02f24414c989e5fa755a8f5b36c59179b2af
Victor verdun VAN DER WATT <vivan-de@e5r8p3.jnb.42.fr>
2016-07-05 16:37:01 +0200

    Created a few of the files and moded header

--- SRCS/own_command.c
+++ SRCS/own_command.c
@@ -1,6 +1,31 @@
+/*
+ *	Things we still need:
+ *		void	ft_echo(char **sa);
+ *		void	ft_cd(char **sa);
+ *		void	ft_export(char **sa);
+ *		void	ft_unset(char **sa);
+ *		void	ft_env(char **sa);
+ */
+
 #include "minishell.h"
 
 int is_own(char *s)
 {
 	return (0);
+}
+
+void	own_command(char **sa)
+{
+	if (ft_memcmp(sa[0], "echo", 5) == 0)
+		ft_echo(sa);
+	else if (ft_memcmp(sa[0], "cd", 3) == 0)
+		ft_cd(sa);
+	else if (ft_memcmp(sa[0], "setenv", 7) == 0)
+		ft_export(sa);
+	else if (ft_memcmp(sa[0], "unsetenv", 9) == 0)
+		ft_unset(sa);
+	else if (ft_memcmp(sa[0], "env", 4) == 0)
+		ft_env(sa);
+	else
+		ft_putstr("is_own is not working properly");
 }
