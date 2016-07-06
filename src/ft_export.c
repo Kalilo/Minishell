320e5be21723281a567abdd0bd7f1ca98f8f894d
Victor verdun VAN DER WATT <vivan-de@e5r9p5.jnb.42.fr>
2016-07-06 16:58:19 +0200

    Working ft_export.c

--- src/ft_export.c
+++ src/ft_export.c
@@ -1,15 +1,17 @@
 #include "../includes/minishell.h"
 
-void	ft_export(char *sa[][], int env_size, char *s)
+void	ft_export(char *sa[], int env_size, char *s)
 {
-	int		i;
 	char	*temp;
 	char	*stemp;
 
-	temp = *sa[env_size - 1];
+	temp = sa[env_size - 1];
 	stemp = s;
 	s = ft_strjoin(s, "\n");
-	*sa[env_size - 1] = ft_strjoin(*sa[env_size], s);
+	if (ft_strcmp(sa[env_size - 1], "#") == 0)
+		sa[env_size - 1] = s;
+	else
+		sa[env_size - 1] = ft_strjoin(sa[env_size], s);
 	free (temp);
 	free(stemp);
 }
