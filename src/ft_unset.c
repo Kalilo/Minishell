5c4a4a325215a33ebd0f3780447a45b62ff65ba0
Kalilo <kalilohansmann@gmail.com>
2016-07-06 15:01:52 +0200

    Added ft_unset

---
+++ src/ft_unset.c
@@ -0,0 +1,44 @@
+#include "../includes/minishell.h"
+
+void	ft_removestr(char *big[], char little[])
+{
+	char	*start;
+	int		k;
+	int		l;
+	char	str[];
+
+	k = -1;
+	str = (char *)malloc(ft_strlen(*big) + 1);
+	start = strstr(big, little);
+	while (*big[++k] != start)
+		str[k] = big[k];
+	l = k;
+	while (*big[l] == start || big[l] == '\n')
+	{
+		start++;
+		l++;
+	}
+	while (big[l] != '\0')
+	{
+		str[k] = big[l];
+		k++;
+		l++;
+	}
+	*big = str;
+}
+
+void	ft_unset(char *sa[][], char var[])
+{
+	int		k;
+	char	temp[];
+
+	k = 0;
+	if (var == NULL)
+		return ;
+	while (*str[k] != NULL)
+	{
+		if (ft_strstr(*sa[k], var) != NULL)
+			ft_removestr(*sa[k], var);
+		k++;
+	}
+}
