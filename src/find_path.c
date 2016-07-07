1732745bb43b1499183cbf5b109f7d38a63a49d8
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-07-07 10:14:41 +0200

    Added basic function layout

---
+++ src/find_path.c
@@ -0,0 +1,18 @@
+#include "../includes/minishell.h"
+
+char	*find_path(t_env *env, char s[])
+{
+	int	k;
+	
+	k = 0;
+	if (s = NULL)
+		return (NULL);
+	while (E_EN[k] != NULL)
+	{
+		if (strstr(E_EN[k], "PATH", ft_strlen(E_EN[k]) != NULL)
+		{
+			//SCAN DIRs
+		}
+		k++;
+	}
+}
