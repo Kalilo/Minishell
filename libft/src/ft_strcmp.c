3b56c282e6bffaadfa1067646e8f5b96f8dda480
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-07-08 15:18:12 +0200

    Got program running for all pre-made commands

--- libft/src/ft_strcmp.c
+++ libft/src/ft_strcmp.c
@@ -3,18 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdebruyn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/06/19 15:48:13 by cdebruyn          #+#    #+#             */
-/*   Updated: 2016/07/07 13:58:41 by cdebruyn         ###   ########.fr       */
+/*   Created: 2016/05/10 09:59:16 by khansman          #+#    #+#             */
+/*   Updated: 2016/05/15 11:09:56 by khansman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
-
-int		ft_strcmp(const char *s1, const char *s2)
+static int	ft_diff(char s1, char s2)
 {
-	while ((int)(*s1++ - *s2++) == 0)
-		;
-	return ((int)(*s1 - *s2));
+	int	l;
+	int	m;
+
+	if (s1 < 0)
+		m = 256 + s1;
+	else
+		m = s1;
+	if (s2 < 0)
+		l = 256 + s2;
+	else
+		l = s2;
+	return (m - l);
+}
+
+int			ft_strcmp(const char *s1, const char *s2)
+{
+	int	k;
+
+	k = 0;
+	while (s1[k] != '\0' && s2[k] != '\0')
+	{
+		if (s1[k] != s2[k])
+			return (ft_diff(s1[k], s2[k]));
+		k++;
+	}
+	if (s1[k] != '\0')
+		return (ft_diff(s1[k], 0));
+	else if (s2[k] != '\0')
+		return (ft_diff(0, s2[k]));
+	return (0);
 }
--- src/find_path.c
+++ src/find_path.c
@@ -1,7 +1,5 @@
 #include "../includes/minishell.h"
 
-#include <string.h>
-
 int		scan_dir(char *s, char *path)
 {
 	DIR				*dir;
@@ -23,22 +21,29 @@ int		scan_dir(char *s, char *path)
 char	*find_path(t_env *env, char *s)
 {
 	int				k;
+	int				l;
+	char			*p;
 	char			**str;
 	
 	k = 0;
-	printf("finding path, s = %s\n", s);//debug
+	l = -1;
+	//printf("finding path, s = %s\n", s);//debug
 	if (s == NULL)
 		return (NULL);
 	while (E_EN[k] != NULL)
 	{
 		
-		if (ft_strstr(E_EN[k], "PATH=") != NULL)
+		if (strstr(E_EN[k], "PATH=") != NULL)//need to use ft_strstr
 		{
-			printf("path found, PATH = %s\n", E_EN[k]);//debug
-			s = ft_strchr(s, '=');
-			s++;
-			printf("%s\n", s);//debug
-			
+			//printf("path found, PATH = %s\n", E_EN[k]);//debug
+			p = ft_strchr(E_EN[k], '=');
+			p++;
+			str = ft_strsplit(p, ':');
+			//ft_env(str);//debug
+			//printf("%s\n", s);//debug
+			while (str[++l] != NULL)
+				if (scan_dir(s, str[l]))
+					return (ft_strjoin(str[l], "/"));			
 		}
 		k++;
 	}
