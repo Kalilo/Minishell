952dc19a36d92ab33607a19d6ecd51a52043957a
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-07-08 14:42:19 +0200

    progress.......

--- src/find_path.c
+++ src/find_path.c
@@ -1,18 +1,46 @@
 #include "../includes/minishell.h"
 
-char	*find_path(t_env *env, char s[])
+#include <string.h>
+
+int		scan_dir(char *s, char *path)
 {
-	int	k;
+	DIR				*dir;
+	struct dirent	*file;
+	
+	dir = opendir(path);
+	while ((file = readdir(dir)) != NULL)
+	{
+		if (ft_strcmp(file->d_name, s) == 0)
+		{
+			closedir(dir);
+			return (1);
+		}
+	}
+	closedir(dir);
+	return (0);
+}
+
+char	*find_path(t_env *env, char *s)
+{
+	int				k;
+	char			**str;
 	
 	k = 0;
-	if (s = NULL)
+	printf("finding path, s = %s\n", s);//debug
+	if (s == NULL)
 		return (NULL);
 	while (E_EN[k] != NULL)
 	{
-		if (strstr(E_EN[k], "PATH", ft_strlen(E_EN[k]) != NULL)
+		
+		if (ft_strstr(E_EN[k], "PATH=") != NULL)
 		{
-			//SCAN DIRs
+			printf("path found, PATH = %s\n", E_EN[k]);//debug
+			s = ft_strchr(s, '=');
+			s++;
+			printf("%s\n", s);//debug
+			
 		}
 		k++;
 	}
+	return (NULL);
 }
