157204a0fad04171cb8d2c3516b51c88b273b092
Cameron DE BRUYN <cdebruyn@e5r8p5.wtc.co.za>
2016-07-10 12:16:14 +0200

    echo

--- libft/src/ft_check_literal.c
+++ libft/src/ft_check_literal.c
@@ -6,11 +6,41 @@
 /*   By: cdebruyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 14:28:08 by cdebruyn          #+#    #+#             */
-/*   Updated: 2016/07/08 10:52:37 by cdebruyn         ###   ########.fr       */
+/*   Updated: 2016/07/10 12:10:41 by cdebruyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+
+char	ft_check_literal2(char a, char b)
+{
+	if (a == 92)
+	{
+		if (b == 92)
+			return ('\\');
+		if (b == '0')
+			return ('\0');
+		if (b == 't')
+			return ('t');
+		if (b == 39)
+			return (39);
+		if (b == 34)
+			return (34);
+		if (b == 'a')
+			return ('a');
+		if (b == 'b')
+			return ('b');
+		if (b == 'f')
+			return ('f');
+		if (b == 'n')
+			return ('n');
+		if (b == 'r')
+			return ('r');
+		if (b == 'v')
+			return ('v');
+	}
+	return (32);
+}
 
 char	ft_check_literal(char a, char b, char c)
 {
@@ -22,10 +52,6 @@ char	ft_check_literal(char a, char b, char c)
 			return ('\'');
 		if (c == 34)
 			return ('\"');
-		if (c == 92)
-			return ('\\');
-		if (c == '0')
-			return ('\0');
 		if (c == 'a')
 			return ('\a');
 		if (c == 'b')
