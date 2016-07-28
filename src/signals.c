41164232f4360b0832addae8bd66836047a11ac4
Kalilo <kalilohansmann@gmail.com>
2016-07-28 09:21:47 +0200

    Eddited signal rules.

--- src/signals.c
+++ src/signals.c
@@ -2,45 +2,26 @@
 
 void sig_handler(int signo)
 {
-	ft_putstr("\e[31mError: an unexpected error occured.\n");
-	exit(0);
-}
-
-void	sigs2(void)
-{
-	signal(SIGTTOU, sig_handler);
-	signal(SIGIO, sig_handler);
-	signal(SIGXCPU, sig_handler);
-	signal(SIGXFSZ, sig_handler);
-	signal(SIGVTALRM, sig_handler);
-	signal(SIGPROF, sig_handler);
-	signal(SIGWINCH, sig_handler);
-	signal(SIGINFO, sig_handler);
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
+	if (signo == SIGHUP || signo == SIGQUIT || signo == SIGABRT ||
+		signo == SIGKILL || signo == SIGTERM || signo == SIGUSR1 || 
+		signo == SIGUSR2)
+	{
+		ft_putstr("\e[31mExitting Program.\n");
+		exit(0);
+	}
+	if (signo == SIGTRAP || signo == SIGBUS ||
+		signo == SIGSEGV || signo == SIGSYS)
+	{
+		ft_putstr("\e[31mError: an unexpected error occured.\n");
+		exit(0);
+	}
 }
 
 void	sigs(void)
 {
-	signal(SIGHUP, sig_handler);
-	signal(SIGINT, sig_handler);
-	signal(SIGQUIT, sig_handler);
-	signal(SIGILL, sig_handler);
-	signal(SIGTRAP, sig_handler);
-	signal(SIGABRT, sig_handler);
-	signal(SIGEMT, sig_handler);
-	signal(SIGFPE, sig_handler);
-	signal(SIGKILL, sig_handler);
-	signal(SIGBUS, sig_handler);
-	signal(SIGSEGV, sig_handler);
-	signal(SIGSYS, sig_handler);
-	signal(SIGPIPE, sig_handler);
-	signal(SIGALRM, sig_handler);
-	signal(SIGTERM, sig_handler);
-	signal(SIGURG, sig_handler);
-	signal(SIGSTOP, sig_handler);
-	signal(SIGTSTP, sig_handler);
-	signal(SIGCHLD, sig_handler);
-	signal(SIGTTIN, sig_handler);
-	sigs2();
+	int		k;
+
+	k = 0;
+	while (++k < 32)
+		signal(k, sig_handler);
 }
