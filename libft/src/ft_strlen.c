8a5ce333e8b2a5a7de7f46699878f5ac2a3d956d
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-08-01 09:59:25 +0200

    Added basic string manipilation

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	cnt;
 
 	cnt = 0;
+	if (s == NULL)
+		return (0);
 	while (s[cnt] != '\0')
 		cnt++;
 	return (cnt);
--- src/com_history.c
+++ src/com_history.c
@@ -5,7 +5,7 @@ int			find_string_pos(char **arr, char *str)
 	int		k;
 
 	k = -1;
-	if (!arr || !arr[k])
+	if (!arr || !arr[k + 1])
 		return (-2);
 	while (arr[++k])
 		if (!ft_strcmp(arr[k], str))
@@ -19,6 +19,8 @@ static void com_hist_add(t_env *env, char *str)
 	char	*s;
 
 	s = (str == NULL) ? ft_strjoin(I_L1, I_L2) : ft_strdup(str);
+	if (ft_strlen(s) == 0)
+		return ;
 	k = -1;
 	while (I_HIS[++k] && k < MAX_HIST)
 		if (!ft_strcmp(I_HIS[k], s))
@@ -26,14 +28,14 @@ static void com_hist_add(t_env *env, char *str)
 			free(s);
 			return ;
 		}
-	if (k == MAX_HIST)
-		free(I_HIS[--k]);
+	if (k == MAX_HIST - 1)
+		free(I_HIS[k]);
 	while (k > 0)
 	{
 		I_HIS[k] = I_HIS[k - 1];
 		k--;
 	}
-	I_HIS[k] = s;
+	I_HIS[0] = s;
 }
 
 static void com_hist_next(t_env *env)
@@ -42,14 +44,23 @@ static void com_hist_next(t_env *env)
 	char	*str;
 
 	str = ft_strjoin(I_L1, I_L2);
-	k = find_string_pos(I_HIS, str) + 1;
-	if (!k || !I_HIS[k])
-		return ;
 	com_hist_add(env, str);
-	free(I_L1);
-	free(I_L2);
+	k = find_string_pos(I_HIS, str) - 1;
+	if (k < 0 || k >= MAX_HIST || !I_HIS[k])
+	{
+		free(str);
+		if (I_L1 == NULL)
+			I_L1 = NULL;
+		I_C1 = 0;
+		return ;
+	}
+	if (I_L1 != NULL)
+		free(I_L1);
+	if (I_L2 != NULL)
+		free(I_L2);
 	I_L1 = I_HIS[k];
 	I_L2 = NULL;
+	I_C1 = ft_strlen(I_L1);
 }
 
 static void com_hist_prev(t_env *env)
@@ -57,15 +68,26 @@ static void com_hist_prev(t_env *env)
 	int		k;
 	char	*str;
 
-	str = ft_strjoin(I_L1, I_L2);
-	k = find_string_pos(I_HIS, str) - 1;
-	if (!k + 1 || !I_HIS[k])
+	if (!I_HIS[0])
 		return ;
+	str = ft_strjoin(I_L1, I_L2);
+	k = find_string_pos(I_HIS, str) + 1;
+	k = (k < 0) ? 0 : k;
 	com_hist_add(env, str);
-	free(I_L1);
-	free(I_L2);
+//	if (I_L1 != NULL)
+//		free(I_L1);
+//	if (I_L2 != NULL)
+//		free(I_L2);
+	if (!I_HIS[k])
+	{
+		if (str != NULL)
+			free(str);
+		return ;
+	}
 	I_L1 = I_HIS[k];
+	I_C1 = ft_strlen(I_L1);
 	I_L2 = NULL;
+	I_C2 = 0;
 }
 
 int			com_history(t_env *env, int action)
