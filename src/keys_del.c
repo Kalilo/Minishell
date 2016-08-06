6b83c704ecbc7e208f61281fc6b10ba470372735
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-08-06 08:23:44 +0200

    Removed comment

--- src/keys_del.c
+++ src/keys_del.c
@@ -7,7 +7,7 @@ void	ke_del(t_env *env)
 	if (I_L2 == NULL || ft_strlen(I_L2) == 0)
 		return ;
 	k = ft_str_inc_dup((I_L2));
-	I_C2 = ft_strlen(I_L2) - 1;//I_C2 = (ft_strlen(I_L2) != 0) ? ft_strlen(I_L2) - 1 : 0;
+	I_C2 = ft_strlen(I_L2) - 1;
 	if (I_L2 != NULL)
 		free(I_L2);
 	I_L2 = k;
