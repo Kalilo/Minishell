534b0a9c353c92561bdabfb49a38d540a69a5698
Kalilo <kalilohansmann@gmail.com>
2016-09-07 12:58:50 +0200

    Cleaned up debugging code.

--- src/check_par.c
+++ src/check_par.c
@@ -6,7 +6,7 @@
 /*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/07 08:55:39 by khansman          #+#    #+#             */
-/*   Updated: 2016/08/07 08:55:41 by khansman         ###   ########.fr       */
+/*   Updated: 2016/09/07 12:58:23 by khansman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,41 +69,6 @@ static int	par_valid(t_par *par)
 	return (1);
 }
 
-static int	check_back_slash(t_env *env, t_par *par)
-{
-/*	char	*pos;
-
-	pos = ft_strchr(P_STR, '\\');
-	printf("cpoint 1\n");//debug
-	if (pos != NULL)
-	{
-		put_bin((unsigned char *)pos);
-		printf("c1-1 pos = '%s'\n", pos);//debug
-		while (++pos)
-		{
-			printf("letter = '%c'\n", *pos);//debug
-			if (ft_isalnum(*pos) && *pos != '\\')
-				return (0);
-		}
-		printf("cpoint 2\n");//debug
-		return (1);
-	}
-	printf("cpoint 3\n");//debug
-	return (0);*/
-	int		k;
-
-	//k = ft_len_until(par->str, 92);
-	printf("k = '%i', str = '%s'\n", k, par->str);
-/*	if (k > 0)
-	{
-		while (P_STR[k++])
-			if (ft_isalnum(P_STR[k]))
-				return (0);
-		return (1);
-	}*/
-	return (0);
-}
-
 int			check_par(t_env *env)
 {
 	t_par	par;
@@ -112,29 +77,11 @@ int			check_par(t_env *env)
 
 	if (I_L1 == NULL && I_L2 == NULL)
 		return (-1);
-	init_par(&par, env);/*
-//	printf("str = '%s'\n", par.str);//debug
-	if ((tmp = ft_strchr(par.str, '\\')) != NULL)
-	{
-		printf("Backslash found, tmp = '%s\n", tmp);//debug
-		//put_bin((unsigned char *)tmp);
-		while (++*tmp)
-			if (ft_isalnum(*tmp))
-				goto x2;
-		printf("Backslash valid\n");//debug
-		free(par.str);
-		result = (0);
-	}
-	else
-	{
-		x2:
-		printf("Normal execution\n");//debug
-		//put_bin((unsigned char *)tmp);*/
-		while (par.str[++(par.k)])
-			par_condition(&par);
-		free(par.str);
-		result = (par_valid(&par));
-//	}
+	init_par(&par, env);
+	while (par.str[++(par.k)])
+		par_condition(&par);
+	free(par.str);
+	result = (par_valid(&par));
 	I_TMP2 = (result) ? 0 : 1;
 	return (result);
 }
