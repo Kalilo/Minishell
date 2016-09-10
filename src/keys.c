78764f347cc71dd46a0275c79ba8892d36b35e22
Kalilo <kalilohansmann@gmail.com>
2016-09-10 18:30:00 +0200

    Added template.

--- src/keys.c
+++ src/keys.c
@@ -12,6 +12,11 @@
 
 #include "../includes/minishell.h"
 
+
+void		toggle_mode(t_env *env)
+{
+}
+
 /*
 **	Had a few difficalties getting the right keys for some values,
 **		thus some of the key actions will have to be assigned to keys of our
@@ -30,6 +35,8 @@ static void	key_handler2(t_env *env, char *key)
 		copy_line(env);
 	else if (!ft_strcmp(K_F17, key))
 		copy_l2(env);
+	else if (!ft_strcmp(K_F15, key))
+		toggle_mode(env);
 }
 
 /*
